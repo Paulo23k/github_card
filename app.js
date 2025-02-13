338d31b05e58be180658d5b1c41e60a7f11a17f4
Paulo Henrique <paulosilvaponted@gmail.com>
2025-02-13 15:23:51 -0300

    update

--- app.js
+++ app.js
@@ -4,8 +4,25 @@ fetch("https://api.github.com/users/Paulo23K")
   .then(resposta => {
     return resposta.json()
   })
-  .then(data => {
-    console.dir(data)
+  .then(dados => {
+    fnCarregarDados(dados.avatar_url, dados.name, dados.bio, dados.email, dados.twitter_username,
+      dados.html_url,
+      dados.blog
+    )
   })
 }
 
+function fnMostrardados(foto, nome, resumo, email, twitter, github,blog) {
+  document.getElementById("avatar").src = foto
+  document.getElementById("nome").innerHTML = nome
+  document.getElementById("resumo").innerHTML = resumo + "<br><strong id='email'></strong>"
+  document.getElementById("email").innerHTML = email
+  document.getElementById("twitter").href = twitter
+  document.getElementById("github").href = github
+  document.getElementById("blog").href = blog
+}
+
+let botao = document.getElementById("botao-carregar-dados")
+botao.addEventListener("click", function(){
+  fnCarregarDados()
+})
